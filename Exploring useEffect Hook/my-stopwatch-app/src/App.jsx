9ec1e8c1fe4df3e68cf967940a6dbfb1d6e87b22
unknown <m.umarfarooq1175@gmail.com>
2025-06-29 22:34:11 +0530

    my-stopwatch-app

--- Exploring useEffect Hook/my-stopwatch-app/src/App.jsx
+++ Exploring useEffect Hook/my-stopwatch-app/src/App.jsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [sece, setCount] = useState(0)
+  const [min,setMinute]=useState(0)
+  const[stop,setStop]=useState(false)
+  useEffect(()=>{
+    let int
+    if(stop){
+      int=setInterval(()=>{
+           setCount((prev)=>{
+            if(prev==59){
+              setMinute(prev=>prev+1)
+              return 0
+            }
+            return prev+1
+           }) 
+        },1000)
+    }
+    return()=> clearInterval(int)
+  },[stop])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{min}:{sece}</h1>
+      <button onClick={()=>setStop(true)}>Start</button>
+       <button onClick={()=>setStop(false)}>Stop</button>
     </>
   )
 }
