72bccc209cb3140e2107ea3e1562bc41ca707231
unknown <m.umarfarooq1175@gmail.com>
2025-07-03 14:59:49 +0530

    pai1

--- pai1/task-app/src/App.jsx
+++ pai1/task-app/src/App.jsx
@@ -1,24 +1,24 @@
 import { useState } from 'react'
 import './App.css'
+import FormInput from "./components/task"
 import { Routes, Route,Link } from 'react-router-dom'
 import task from "./components/task";
 import about from "./components/about"
 import landingpage from "./landingpage";
 
 
-function App() {
-  
+function App() {  
   return (
     <>
-  
+   <task/>
+  <FormInput/>
     <nav>
       <Link to="/components/task">task</Link>
       <br />
       <br />
       <Link to="/components/about">about</Link>
-      
-      <Routes>
 
+      <Routes>
         <Route path="/" element ={< landingpage/>} />
          <Route path="/components/about" element ={< about/>} />
           <Route path="/components/task" element ={< task/>} />        
--- pai1/task-app/src/components/task.jsx
+++ pai1/task-app/src/components/task.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
+import React, { useEffect, useReducer, useState } from "react";
 
-const task = () => {
+let newtask = {
+  title:"",
+  description:"",
+  
+};
+
+
+export function FormInput() {
+  let [task, tasklist] = useState([]);
+
+  function handleChange(event) {
+    
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    tasklist([...task, newtask]);  
+  }
   return (
-    <div>
-      <h1>task list</h1>
-      
-    </div>
-  )
+    <>
+      <h3>Create list..</h3>
+      <div id="formInput">
+        <form onSubmit={handleSubmit}>
+          <label>
+            <input
+              type="text"
+              name="title"
+              value={task.title}
+              placeholder="Enter Title"
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            <input
+              type="text"
+              name="description"
+              value={task.description}
+              placeholder="Enter the description"
+              onChange={handleChange}
+            />
+          </label>    
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default task
+export default task;
+
+
+
